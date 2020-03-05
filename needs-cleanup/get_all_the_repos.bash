0c8532c6c5cfffbeedbc351bd788a389441a8af8
gustafc <gustafc@gmail.com>
2020-03-05 09:59:05 +0100

    Cleanup

--- needs-cleanup/get_all_the_repos.bash
+++ needs-cleanup/get_all_the_repos.bash
@@ -10,7 +10,7 @@ for repo in $repos; do
     if [[ -d $repo ]]; then
 	echo $repo already cloned.
     else
-        (set -x; git clone --recursive "git@github.com:sbs-discovery-sweden/$repo.git")
+        (set -x; git clone --recursive "TODO: URL to $repo.git")
     fi
 done
 bash ./apply_email.bash
